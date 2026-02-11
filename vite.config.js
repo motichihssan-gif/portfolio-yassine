4e8c347c5a0eb6696b35a46aa9f9a6c87b07206e
yassineihsane22-ai <yassineihsane22@gmail.com>
2026-02-11 01:09:38 +0100

    Mise à jour des certificats et du README

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    // Si vous déployez sur GitHub Pages, décommentez la ligne ci-dessous
+    // base: '/portfolio-yassine/',
+})
